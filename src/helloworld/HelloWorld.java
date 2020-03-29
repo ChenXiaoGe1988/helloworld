d46038387b6c162ad9d1eb2dc422938984443862
18374087695 <18374087695@DESKTOP-VLUHCV3>
2020-03-29 14:47:19 +0800

    test

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -3,7 +3,7 @@ package helloworld;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello CodeChen");
+		System.out.println("Hello CodeChen ! This is you first git program!");
 	}
 
 }
